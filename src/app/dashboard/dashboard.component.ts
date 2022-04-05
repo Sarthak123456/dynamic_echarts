0f269d497c0c9fa3d101e2673a008d600da010f4
Sarthak <sarthak.tuteja@yahoo.com>
2022-04-06 03:19:29 +0530

    implemented drag and drop in chart ans fixed css

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,54 +1,91 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { FormControl, FormGroup } from '@angular/forms';
+import * as echarts from 'echarts';
+
+
+
 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  // changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewChecked {
   @ViewChild('openModal') openModal!:ElementRef<any>;
+
 
   house = faHouse;
   chartOptions:any;
   mergeOptions = {};
-  count = 1000;
-  str='sd'
-  data = [0,820, 932]
-  echartsInstance: any;
+  position:any;
+  data = [0,200, 300]
+  mychart: any;
   myForm!: FormGroup;
+  down = "path://M18.121,9.88l-7.832-7.836c-0.155-0.158-0.428-0.155-0.584,0L1.842,9.913c-0.262,0.263-0.073,0.705,0.292,0.705h2.069v7.042c0,0.227,0.187,0.414,0.414,0.414h3.725c0.228,0,0.414-0.188,0.414-0.414v-3.313h2.483v3.313c0,0.227,0.187,0.414,0.413,0.414h3.726c0.229,0,0.414-0.188,0.414-0.414v-7.042h2.068h0.004C18.331,10.617,18.389,10.146,18.121,9.88 M14.963,17.245h-2.896v-3.313c0-0.229-0.186-0.415-0.414-0.415H8.342c-0.228,0-0.414,0.187-0.414,0.415v3.313H5.032v-6.628h9.931V17.245z M3.133,9.79l6.864-6.868l6.867,6.868H3.133z";
 
+  path = [this.down]
 
-
-  constructor(private cd:ChangeDetectorRef) {
-    // setInterval(() => {
-    //   console.log('updated');
-    //   this.add();
-    //   console.log(this.chartOption.series[0].data);
-    //   console.log(this.chartOption.xAxis.data);
-
-    //   // let newData = this.chartOption.series[0].data;
-
-    //   // newData.push('sads');
-
-    //   this.mergeOptions = this.chartOption;
-    //   this.cd.detectChanges();
-
-    // },5000);
-
+  constructor() {
   }
 
   ngOnInit(): void {
-    // this.add();
     this.myForm = new FormGroup({
       name: new FormControl('Thur'),
-      amount: new FormControl(1000),
+
+
+      amount: new FormControl(this.amount),
     });
+
+    setTimeout(() => {
+      this.mychart.setOption({
+        graphic: echarts.util.map(this.data,  (item, dataIndex:any) => {
+            return {
+                type: 'circle',
+                position: this.mychart.convertToPixel('grid', [dataIndex,item]),
+                shape: {
+                    cx: 0,
+                    cy: 0,
+                    r: 20 / 2
+                },
+                invisible: true,
+                draggable: true,
+                ondrag: echarts.util.curry(this.onPointDragging, dataIndex,this.mychart,this.data),
+                z: 100
+            };
+        })
+    });
+    }, 100)
+
   }
+  ngAfterViewChecked(): void {
+
+    setTimeout(() => {
+      this.mychart.setOption({
+        graphic: echarts.util.map(this.data,  (item, dataIndex:any) => {
+            return {
+                type: 'circle',
+                position: this.mychart.convertToPixel('grid', [dataIndex,item]),
+                shape: {
+                    cx: 0,
+                    cy: 0,
+                    r: 20 / 2
+                },
+                invisible: true,
+                draggable: true,
+                ondrag: echarts.util.curry(this.onPointDragging, dataIndex,this.mychart,this.data),
+                // onmousemove: echarts.util.curry(this.showTooltip, dataIndex, this.mychart),
+                // onmouseout: echarts.util.curry(this.hideTooltip, dataIndex, this.mychart),
+                z: 100
+            };
+        })
+    });
+    }, 100)
+
+  }
+
   onSubmit(form: FormGroup) {
     this.add( this.amount , form.value.name);
   }
@@ -58,7 +95,7 @@ export class DashboardComponent implements OnInit {
   done2 = [];
   done3 = [];
   value = 0;
-  amount=1000;
+  amount=100;
 
   handleMinus() {
     this.amount--;
@@ -73,7 +110,6 @@ export class DashboardComponent implements OnInit {
         formatter: '{value}',
         align: 'center'
       },
-
       type: 'category',
       data: ['Mon', 'Tue', 'Wed']
     },
@@ -92,15 +128,24 @@ export class DashboardComponent implements OnInit {
     series: [
       {
         data: this.data,
+        id:'a',
         type: 'line',
-        smooth: true
+        smooth: true,
+        symbol: () => {
+          return this.path[0]
+        },
+        symbolSize: [20, 30],
+        symbolOffset: [0, 0],
+        // color: 'orange',
+        symbolPosition: 'start',
+        fontSize: 300,
+        fontWeight: 'bold',
+        color: '#34DCFF'
       }
     ]
   };
 
   drop(event: CdkDragDrop<any>) {
-    console.log('event = '  , event );
-    console.log('event = '  , event.container.data, event.previousIndex, event.currentIndex );
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else if(event.previousContainer !== event.container){
@@ -115,32 +160,75 @@ export class DashboardComponent implements OnInit {
   }
 
   onChartInit(ec: any) {
-    this.echartsInstance = ec;
+    this.mychart = ec;
   }
 
   resizeChart() {
-    if (this.echartsInstance) {
-      this.echartsInstance.resize();
+    if (this.mychart) {
+      this.mychart.resize();
     }
   }
 
   add(amount:number, name:string){
 
-    // this.chartOption = this.chartOption;
     if(this.chartOption.series[0].type === 'line'){
-      // this.str = 'k'+ this.str;
       this.chartOption.xAxis.data.push(name);
       this.data.push(amount);
 
     }
-    this.mergeOptions = {};
-    this.mergeOptions = this.chartOption;
-    this.echartsInstance.setOption(this.chartOption, true, true);
-    this.resizeChart()
+    this.mychart.setOption(this.chartOption, true, true);
 
-    // console.log(this.chartOption);
-    console.log(this.mergeOptions);
-    // this.cd.detectChanges();
+      // Add shadow circles (which is not visible) to enable drag.
+      setTimeout(() => {
+        this.mychart.setOption({
+          graphic: echarts.util.map(this.data,  (item, dataIndex:any) => {
+            // console.log(this.mychart.convertToPixel({seriesIndex:0}, [0,200]));
+              return {
+                  type: 'circle',
+                  position: this.mychart.convertToPixel('grid', [dataIndex,item]),
+                  shape: {
+                      cx: 0,
+                      cy: 0,
+                      r: 20 / 2
+                  },
+                  invisible: true,
+                  draggable: true,
+                  ondrag: echarts.util.curry(this.onPointDragging, dataIndex,this.mychart,this.data),
+                  // onmousemove: echarts.util.curry(this.showTooltip, dataIndex, this.mychart),
+                  // onmouseout: echarts.util.curry(this.hideTooltip, dataIndex, this.mychart),
+                  z: 100
+              };
+          })
+      });
+      }, 100)
+
+
+
+  }
+  onPointDragging(dataIndex:any, mychart:any, data:any, position:any):any {
+      const pointInPixel= [position.event.layerX, position.event.layerY];
+
+          data[dataIndex] =   pointInPixel[1] ? pointInPixel[1] : 0;
+
+          // Update data
+          mychart.setOption({
+              series: [{
+                  id: 'a',
+                  data: data
+              }]
+          });
+      mychart.resize()
+  }
+
+  showTooltip(dataIndex:any, mychart:any) {
+    mychart.dispatchAction({
+      type: 'showTip',
+      seriesIndex: 0,
+      dataIndex: dataIndex
+    });
+  }
+  hideTooltip(dataIndex:any, mychart:any) {
+    mychart.dispatchAction({ type: 'hideTip' });
   }
 
 
